cc474736e706d90281ca8f13bd4faef1b9ac954c
omkar273 <omkarsonawane622@gmail.com>
2024-02-01 11:55:43 +0530

    remote extra code and files

--- src/pages/ErrorPage/ErrorPage.jsx
+++ src/pages/ErrorPage/ErrorPage.jsx
@@ -1,4 +1,3 @@
-import React from 'react';
 import { Box, Button, Container, Typography } from '@mui/material';
 import Grid from '@mui/material/Grid';
 import { useNavigate } from 'react-router-dom';
